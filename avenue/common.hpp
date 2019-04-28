3ccabceebea3846972923d59b9c0c49f75900def
miaozhongkai.aiyowoo <miaozhongkai.aiyowoo@shandagames.com>
2019-04-28 20:02:55 +0800

    common.hpp 在server_connnection和client_connection之间共享

---
+++ avenue/common.hpp
@@ -0,0 +1,19 @@
+#ifndef AVENUE_COMMON_HPP
+#define AVENUE_COMMON_HPP
+
+/*
+ * heart beat service id
+ */
+constexpr uint32_t HEART_BEAT_SERVICE_ID = 0;
+
+/*
+ * heart beat message id
+ */
+constexpr uint32_t HEART_BEAT_MESSAGE_ID = 0;
+
+/*
+ * heart beat timeout seconds
+ */
+constexpr uint32_t HEART_BEAT_TIMEOUT_SECONDS = 2;
+
+#endif
